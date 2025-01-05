2c676fd709af58f91cc2106d259c2aa0022007a0
CruzTotvs <edinei.pereira@totvs.com.br>
2025-01-05 19:10:39 -0300

    :rocket:Implementação Brutal Force Simples, versão 1.0 Beta

---
+++ brutalForce.java
@@ -0,0 +1,33 @@
+public class brutalForce {
+//    Escreva um programa que implemente uma quebra de senha utilizando o método de força bruta. O programa deve:
+//
+//    Solicitar que o usuário digite uma senha de 4 dígitos positivos.
+//    Testar todas as combinações possíveis de números entre 0000 e 9999 até descobrir qual é a senha.
+//    Ao encontrar a senha:
+//    Exibir o número de tentativas realizadas.
+//    Mostrar o tempo total gasto para quebrar a senha.
+//            Notas
+//    Para capturar o tempo atual em milissegundos, utilize o seguinte trecho de código:
+//            System.currentTimeMillis();
+//    Para calcular o tempo gasto, subtraia o tempo final do tempo inicial.
+
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis ();
+        int senha = 1986;
+        int tentativas = 0;
+
+        for (int i = 0; i < 10000; i++) {
+            if (i == senha) {
+                System.out.println ( "Senha Localizada: " + i );
+                break;
+            }
+            tentativas++;
+        }
+        long fim = System.currentTimeMillis ();
+        long tempoGasto = ( fim - inicio );
+        System.out.println ( "Tentativas: " + tentativas );
+        System.out.println ( "Tempo gasto: " + tempoGasto );
+    }
+
+}
+
