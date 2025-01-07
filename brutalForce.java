3fe34d5936f7999baa1cb8f8deda19958d278b1b
CruzTotvs <edinei.pereira@totvs.com.br>
2025-01-06 21:56:51 -0300

    :bug:Implementação Brutal Force Simples, versão 1.3 validate

--- brutalForce.java
+++ brutalForce.java
@@ -17,33 +17,54 @@ public class brutalForce {
 
         Scanner entrada = new Scanner (System.in);
 
-        long inicio = System.currentTimeMillis ();
 
         // Validar a senha digitada pelo usuario, se tem 4 digitos e é positiva
-        int senha = 0;
+        String senha = "";
+
         while (true){
             System.out.println("Informe uma senha com quatro digitos positivos: ");
-             senha = entrada.nextInt ();
-            if (senha >= 1000  && senha <= 9999) {
+             senha = entrada.nextLine ();
+            if (senha.length () == 4 ) {
                 break;
             }
+            for (int i = 0; i < senha.length (); i++) {
+                if ( senha.charAt ( i ) < '0' || senha.charAt ( i ) > '9' ) {
+                    System.out.println ( "Senha invalida, digite novamente" );
+                }
+            }
         }
-
 
         int tentativas = 0;
 
-        for (int i = 0; i < 10000; i++) {
-            if (i == senha) {
-                System.out.println ( "Senha Localizada: " + i );
-                break;
+        long inicio = System.currentTimeMillis ();
+        String senhaLocalizada = "";
+//        for ( int i = 0; i < senha.length (); i++ ) {
+//            // Validar se a senha é igual a senha digitada pelo usuario
+//            for ( int j = 0; j < 10; j++ ) {
+//                if (j == senha.charAt(i)) {
+//                    senhaLocalizada = senhaLocalizada + j;
+//                    System.out.println ( "Senha Localizada: " + senhaLocalizada );
+//                    break;
+//                }
+//            }
+//
+//            }
+        for (int i = 0; i < senha.length(); i++) {
+            for (int j = 0; j < 10; j++) {
+                tentativas++;
+                if (Character.forDigit(j, 10) == senha.charAt(i)) {
+                    senhaLocalizada = senhaLocalizada + j;
+                    break;
+                }
             }
-            tentativas++;
         }
+
         long fim = System.currentTimeMillis ();
         long tempoGasto = ( fim - inicio );
 
         System.out.println ( "Tentativas: " + tentativas );
         System.out.println ( "Tempo gasto: " + tempoGasto + " Ms " );
+        System.out.println ( "Senha Localizada: " + senhaLocalizada );
 
         entrada.close ();
     }
